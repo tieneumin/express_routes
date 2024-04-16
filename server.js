70fde7110ca407cc34906d301ec9896e4b4a3a29
Tien Eu Min <tieneumin@gmail.com>
2024-04-16 14:46:12 +0800

    repo rename push test

--- server.js
+++ server.js
@@ -57,6 +57,6 @@ app.get("/authors/:id", (req, res) => {
 });
 // END OF ANSWER
 
-app.listen(5001, () => {
-  console.log("Bookstore app is running on port 5001");
+app.listen(5000, () => {
+  console.log("Bookstore app is running on port 5000");
 });
